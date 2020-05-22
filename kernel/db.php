9a4e39476778f0ba258dab2f00641ba993ead8ac
savchenko <s.savchenko@outlook.com>
2020-05-23 00:01:29 +0700

    Db connection.

--- kernel/db.php
+++ kernel/db.php
@@ -8,10 +8,10 @@ $capsule = new Manager();
 
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'homestead',
-    'username'  => 'homestead',
-    'password'  => 'secret',
+    'host'      => getenv('db_host'),
+    'database'  => getenv('db_name'),
+    'username'  => getenv('db_username'),
+    'password'  => getenv('db_password'),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',

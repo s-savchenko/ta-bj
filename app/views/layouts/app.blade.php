7e56ec5fd654e2052df83edf0fc3da5aaf602b7d
savchenko <s.savchenko@outlook.com>
2020-05-23 00:31:18 +0700

    Remove custom.css link.

--- app/views/layouts/app.blade.php
+++ app/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Тестовое задание</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link rel="stylesheet" href="/assets/css/custom.css">
 </head>
 <body class="d-flex flex-column h-100">
 <main role="main" class="flex-shrink-0">

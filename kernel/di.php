e441fb65d3495f90e4e83370477b4122d400a68e
savchenko <s.savchenko@outlook.com>
2020-05-22 14:58:34 +0700

    Dependency injection: add psr-7 request & response.

--- kernel/di.php
+++ kernel/di.php
@@ -1,8 +1,15 @@
 <?php
 
 use DI\ContainerBuilder;
+use Laminas\Diactoros\ResponseFactory;
+use Laminas\Diactoros\ServerRequestFactory;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 $containerBuilder = new ContainerBuilder();
-$containerBuilder->addDefinitions([]);
+$containerBuilder->addDefinitions([
+    ServerRequestInterface::class => fn () => ServerRequestFactory::fromGlobals(),
+    ResponseInterface::class => fn () => (new ResponseFactory())->createResponse(),
+]);
 
 return $containerBuilder->build();

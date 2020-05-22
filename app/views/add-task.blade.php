81511cc7cb457db537eda63366eec9a6b21b3c09
savchenko <s.savchenko@outlook.com>
2020-05-22 18:12:16 +0700

    Add task: validation.

--- app/views/add-task.blade.php
+++ app/views/add-task.blade.php
@@ -10,16 +10,16 @@
             <label for="nameInput">Имя пользователя</label>
             <input type="text" class="form-control @if(isset($userNameFailed) && $userNameFailed) is-invalid @endif"
                    id="nameInput" placeholder="Имя пользователя" name="user_name"
-                   value="{{ isset($userName) ? $userName : '' }}">
+                   value="{{ isset($userName) ? $userName : '' }}" required>
             @if (isset($userNameFailed) && $userNameFailed)
                 <div class="text-danger"><small>Поле "Имя пользователя" обязательно для заполнения!</small></div>
             @endif
         </div>
         <div class="form-group">
             <label for="emailInput">Email</label>
-            <input type="text" class="form-control @if(isset($emailFailed) && $emailFailed) is-invalid @endif"
+            <input type="email" class="form-control @if(isset($emailFailed) && $emailFailed) is-invalid @endif"
                    id="emailInput" placeholder="Email" name="email"
-                   value="{{ isset($email) ? $email : '' }}">
+                   value="{{ isset($email) ? $email : '' }}" required>
             @if (isset($emailFailed) && $emailFailed)
                 <div class="text-danger">
                     <small>Поле "Email" обязательно для заполнения и должно иметь формат xxx@xxx.xx!</small>
@@ -29,7 +29,7 @@
         <div class="form-group">
             <label for="contentTextarea">Текст задачи</label>
             <textarea class="form-control @if(isset($contentFailed) && $contentFailed) is-invalid @endif"
-                      id="contentTextarea" rows="3" name="content">{{ isset($content) ? $content : '' }}</textarea>
+                      id="contentTextarea" rows="3" name="content" required>{{ isset($content) ? $content : '' }}</textarea>
             @if (isset($contentFailed) && $contentFailed)
                 <div class="text-danger"><small>Поле "Текст задачи" обязательно для заполнения!</small></div>
             @endif

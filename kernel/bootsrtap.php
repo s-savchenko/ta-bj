7ec39d7b04f3668c4950cc5b1d79d5e76a9fcc8d
savchenko <s.savchenko@outlook.com>
2020-05-22 14:44:44 +0700

    App bootstrap: add container.

--- kernel/bootsrtap.php
+++ kernel/bootsrtap.php
@@ -1,4 +1,9 @@
 <?php
 
+use Psr\Container\ContainerInterface;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/errorHandler.php';
+
+/** @var ContainerInterface $container */
+$container = require __DIR__ . '/di.php';

0f5836e378dfa6d3a9ecfa7a24858d0990eff0bc
savchenko <s.savchenko@outlook.com>
2020-05-22 17:00:12 +0700

    Add task page: validation.

--- app/Controllers/AddTaskController.php
+++ app/Controllers/AddTaskController.php
@@ -12,4 +12,25 @@ class AddTaskController extends Controller
 
         return $this->response;
     }
+
+    public function store()
+    {
+        $parsedBody = $this->request->getParsedBody();
+        $userName = isset($parsedBody['user_name']) ? trim($parsedBody['user_name']) : '';
+        $email = isset($parsedBody['email']) ? trim($parsedBody['email']) : '';
+        $content = isset($parsedBody['content']) ? trim($parsedBody['content']) : '';
+
+        $userNameFailed = $userName === '';
+        $emailFailed = $email === '';
+        $contentFailed = $content === '';
+
+        if ($userNameFailed || $emailFailed || $contentFailed) {
+            $this->response->getBody()->write(
+                $this->blade->render('add-task', compact('userName', 'email', 'content', 'userNameFailed',
+                    'emailFailed', 'contentFailed'))
+            );
+        }
+
+        return $this->response;
+    }
 }
--- app/views/add-task.blade.php
+++ app/views/add-task.blade.php
@@ -5,18 +5,32 @@
         <a href="#" class="text-muted">Вход для администратора</a>
     </div>
     <h1 class="my-5 text-center">Добавить задачу</h1>
-    <form class="mb-4">
+    <form class="mb-4" method="post">
         <div class="form-group">
             <label for="nameInput">Имя пользователя</label>
-            <input type="text" class="form-control" id="nameInput" placeholder="Имя пользователя">
+            <input type="text" class="form-control @if(isset($userNameFailed) && $userNameFailed) is-invalid @endif"
+                   id="nameInput" placeholder="Имя пользователя" name="user_name"
+                   value="{{ isset($userName) ? $userName : '' }}">
+            @if (isset($userNameFailed) && $userNameFailed)
+                <div class="text-danger"><small>Поле "Имя пользователя" обязательно для заполнения!</small></div>
+            @endif
         </div>
         <div class="form-group">
             <label for="emailInput">Email</label>
-            <input type="email" class="form-control" id="emailInput" placeholder="Email">
+            <input type="email" class="form-control @if(isset($emailFailed) && $emailFailed) is-invalid @endif"
+                   id="emailInput" placeholder="Email" name="email"
+                   value="{{ isset($email) ? $email : '' }}">
+            @if (isset($emailFailed) && $emailFailed)
+                <div class="text-danger"><small>Поле "Email" обязательно для заполнения!</small></div>
+            @endif
         </div>
         <div class="form-group">
-            <label for="taskTextarea">Текст задачи</label>
-            <textarea class="form-control" id="taskTextarea" rows="3"></textarea>
+            <label for="contentTextarea">Текст задачи</label>
+            <textarea class="form-control @if(isset($contentFailed) && $contentFailed) is-invalid @endif"
+                      id="contentTextarea" rows="3" name="content">{{ isset($content) ? $content : '' }}</textarea>
+            @if (isset($contentFailed) && $contentFailed)
+                <div class="text-danger"><small>Поле "Текст задачи" обязательно для заполнения!</small></div>
+            @endif
         </div>
         <button type="submit" class="btn btn-primary">Сохранить</button>
     </form>

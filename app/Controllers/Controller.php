a987eb8d0fbc1848a4c0843670b67cdd529443d4
savchenko <s.savchenko@outlook.com>
2020-05-22 15:44:46 +0700

    Add blade as a protected property to base controller.

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -4,7 +4,7 @@
 namespace App\Controllers;
 
 
-use Philo\Blade\Blade;
+use Jenssegers\Blade\Blade;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -12,10 +12,12 @@ abstract class Controller
 {
     protected ServerRequestInterface $request;
     protected ResponseInterface $response;
+    protected Blade $blade;
 
-    public function __construct(ServerRequestInterface $request, ResponseInterface $response)
+    public function __construct(ServerRequestInterface $request, ResponseInterface $response, Blade $blade)
     {
         $this->request = $request;
         $this->response = $response;
+        $this->blade = $blade;
     }
 }
